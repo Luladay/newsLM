ba9754485d673034d5680f977b14d3f9ced83f44
Luladay Price <luladayp@rice08.stanford.edu>
2018-03-09 14:48:56 -0800

    Updated baseline.py, modified data matrix code

--- baseline.py
+++ baseline.py
@@ -1,81 +1,129 @@
 import pickle
 import tensorflow as tf
 import numpy as np
-import matplotlib
-matplotlib.use('agg')
-import matplotlib.pyplot as plt
-from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import confusion_matrix
-
-#Confusion matrix function from PA1, Trying to adapt for our purposes
-def outputConfusionMatrix(features, labels, clf, filename):
-    """ Generate a confusion matrix """
-    pred = clf.predict(features)
-    cm = confusion_matrix(labels, pred, labels=range(5))
-    plt.figure()
-    plt.imshow(cm, interpolation='nearest', cmap=plt.cm.Reds)
-    plt.colorbar()
-    classes = ["- -", "-", "neut", "+", "+ +"]
-    tick_marks = np.arange(len(classes))
-    plt.xticks(tick_marks, classes)
-    plt.yticks(tick_marks, classes)
-    thresh = cm.max() / 2.
-    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
-        plt.text(j, i, cm[i, j],
-                 horizontalalignment="center",
-                 color="white" if cm[i, j] > thresh else "black")
-    plt.tight_layout()
-    plt.ylabel('True label')
-    plt.xlabel('Predicted label')
-    plt.savefig(filename)
-# change the filenames if the pickle files are in a Data folder
-train_matrix = util.openPkl("train_matrix_short.pkl")
-train_labels = util.openPkl("train_labels_short.pkl")
-dev_matrix = util.openPkl("dev_matrix_short.pkl")
-dev_labels = util.openPkl("dev_labels_short.pkl")
-test_matrix = util.openPkl("test_matrix_short.pkl")
-test_labels = util.openPkl("test_labels_short.pkl")
+import util
 
 
+def run_baseline(data_matrix, data_labels, train=True):
+	n_features = util.glove_dimensions
+	n_classes = 5
+	batch_size = 1000
+	n_epochs = 30
+	lr = 0.005
+
+	input_placeholder = tf.placeholder(tf.float32, shape=(None, n_features))
+	labels_placeholder = tf.placeholder(tf.int32, shape=(None, n_classes))
+
+	W = tf.Variable(tf.zeros([n_features, n_classes], dtype=tf.float32))
+	b = tf.Variable(tf.zeros([n_classes, 1], dtype=tf.float32))
+
+	xW = tf.matmul(input_placeholder, W)
+	pred = tf.transpose(tf.transpose(xW) + b)
+
+	loss_op = tf.nn.softmax_cross_entropy_with_logits(labels=labels_placeholder, logits=pred)
+	loss_op = tf.reduce_mean(loss_op, 0) 
+
+	train_op = tf.train.AdamOptimizer(learning_rate = lr).minimize(loss_op)
+
+	with tf.Session() as sess:
+		sess.run(tf.global_variables_initializer())
+
+		minibatches = get_minibatches(data_matrix, data_labels, batch_size)
+		for i in range(n_epochs):
+			print "Epoch " + str(i+1) + ": "
+			loss_list = []
+			for tup in minibatches:
+				if train:
+					_, loss = sess.run([train_op, loss_op], feed_dict={input_placeholder: tup[0], labels_placeholder: tup[1]})
+				else:
+					loss = sess.run(loss_op, feed_dict={input_placeholder: tup[0], labels_placeholder: tup[1]})
+				print loss
+				# if np.isnan(loss):
+				# 	print "input: ", input_placeholder
+				# 	print "labels: ", labels_placeholder
+				loss_list.append(loss)
+			print "=====>loss: " + str(np.mean(loss_list)) + " "
+		
+
+def get_minibatches(data_matrix, data_labels, batch_size):
+	batch_list = []
+	indices = []
+	n_matrix_rows = data_matrix.shape[0]
+	for i in range(0, n_matrix_rows, batch_size):
+		batch = data_matrix[i : i+batch_size, : ]
+		batch_label = data_labels[i : i+batch_size]
+		batch_list.append((batch, batch_label))
+	return batch_list
+
+def test_minibatches():
+	data_matrix = np.arange(20).reshape((10,2))
+	data_labels = np.arange(10)
+	batch_size = 4
+	print "data_matrix: ", data_matrix
+	print ""
+	print "data_labels: ", data_labels
+	print ""
+	batches = get_minibatches(data_matrix, data_labels, batch_size)
+	for tup in batches:
+		print tup
 
 
-n_samples = train_matrix.shape[0]
-n_features = train_matrix.shape[1] #should be util.glove_dimensions
-n_classes = 5
-batch_size = 60
-n_epochs = 50
-lr = 0.001
+if __name__ == '__main__':
+	# change these filenames if the pickle files are in a Data folder
+	# print "Opening train data..."
+	# train_matrix = util.openPkl("train_matrix_short.pkl")
+	# train_labels = util.openPkl("train_labels_short.pkl")
+	# print "Done opening training data!"
+	# run_baseline(train_matrix, train_labels)
 
 
-input_placeholder = tf.placeholder(tf.float32, shape=(n_samples, n_features))
-labels_placeholder = tf.placeholder(tf.int32, shape=(1, n_classes))
+	# print "Opening test data..."
+	# test_matrix = util.openPkl("test_matrix_short.pkl")
+	# test_labels = util.openPkl("test_labels_short.pkl")
+	# print "Done opening test data!"
+	# batch_list = get_minibatches(test_matrix, test_labels, 1000)
+	# batch_batches = get_minibatches(batch_list[3][0], np.arange(1000), 100)	
+	# batch_problems = get_minibatches(batch_batches[5][0], np.arange(100), 10)
+	# more_batches = get_minibatches(batch_problems[2][0], np.arange(10), 1)
+	
+	# print more_batches[0][0]
 
-W = tf.Variable(tf.zeros([n_features, n_classes], dtype=tf.float32))
-b = tf.Variable(tf.zeros([n_classes, 1], dtype=tf.float32))
+	print "Opening train matrix..."
+	train_matrix = util.openPkl("train_matrix_short.pkl")
+	print "Done opening test matrix!"	
+	for i, row in enumerate(train_matrix):
+		if np.isnan(row).any():
+			print "row num: ", i
+			print "row values: ", row
+			print " "
+			
+	# print "Opening test_matrix matrix..."
+	# test_matrix = util.openPkl("test_matrix_short.pkl")
+	# print "Done opening test matrix!"	
+	# for i, row in enumerate(test_matrix):
+	# 	if np.isnan(row).any():
+	# 		print "row num: ", i
+	# 		print "row values: ", row
+	# 		print " "
 
-xW = tf.matmul(input_placeholder, W)
-pred = tf.transpose(tf.transpose(xW) + b)
 
-loss = tf.nn.softmax_cross_entropy_with_logits(labels=self.labels_placeholder, logits=pred)
-loss = tf.reduce_mean(loss, 0) #not sure if we only needed this before in the hw b/c we were training in batches??
+	print "Opening dev matrix..."
+	dev_matrix = util.openPkl("dev_matrix_short.pkl")
+	print "Done opening dev matrix!"	
+	for i, row in enumerate(dev_matrix):
+		if np.isnan(row).any():
+			print "row num: ", i
+			print "row values: ", row
+			print " "
+	
 
 
 
-train_op = tf.train.GradientDescentOptimizer(learning_rate = lr).minimize(loss)
+	# run_baseline(test_matrix, test_labels, train=False)
 
 
-
-with tf.Graph().as_default() as graph:
-	# Build the model and add the variable initializer op
-	init_op = tf.global_variables_initializer()
-	# Finalizing the graph causes tensorflow to raise an exception if you try to modify the graph
-	# further. This is good practice because it makes explicit the distinction between building and
-	# running the graph.
-	graph.finalize()
-
-	# Create a session for running ops in the graph
-	with tf.Session(graph=graph) as sess:
-		# Run the op to initialize the variables.
-		sess.run(init_op)
-		# Fit the model
-		losses = model.fit(sess, inputs, labels)
+	
+	# print "Opening dev data..."
+	# dev_matrix = util.openPkl("dev_matrix_short.pkl")
+	# dev_labels = util.openPkl("dev_labels_short.pkl")
+	# print "Done opening dev data!"
--- create_data_matrices_baseline.py
+++ create_data_matrices_baseline.py
@@ -37,19 +37,19 @@ def build_matrices(embed_matrix_filename, vocab_dict_filename, short=True):
 		test_data_filename = "Data/data_test_full.pkl"
 		dev_data_filename = "Data/data_dev_full.pkl"
 
-	train_matrix, train_labels = build_matrices_helper(train_data_filename, vocab_dict, embed_matrix)
-	test_matrix, test_labels = build_matrices_helper(test_data_filename, vocab_dict, embed_matrix)
+	# train_matrix, train_labels = build_matrices_helper(train_data_filename, vocab_dict, embed_matrix)
+	# test_matrix, test_labels = build_matrices_helper(test_data_filename, vocab_dict, embed_matrix)
 	dev_matrix, dev_labels = build_matrices_helper(dev_data_filename, vocab_dict, embed_matrix)
 	
 	
-	util.dumpVar(train_matrix_pkl + ".pkl", train_matrix)
-	util.dumpVar(train_labels_pkl + ".pkl", train_labels)
+	# util.dumpVar(train_matrix_pkl + ".pkl", train_matrix)
+	# util.dumpVar(train_labels_pkl + ".pkl", train_labels)
 
-	util.dumpVar(test_matrix_pkl + ".pkl", test_matrix)	
-	util.dumpVar(test_labels_pkl + ".pkl", test_labels)
+	# util.dumpVar(test_matrix_pkl + ".pkl", test_matrix)	
+	# util.dumpVar(test_labels_pkl + ".pkl", test_labels)
 
-	util.dumpVar(dev_matrix_pkl + ".pkl", dev_matrix)
-	util.dumpVar(dev_labels_pkl + ".pkl", dev_labels)
+	# util.dumpVar(dev_matrix_pkl + ".pkl", dev_matrix)
+	# util.dumpVar(dev_labels_pkl + ".pkl", dev_labels)
 
 
 
@@ -61,34 +61,45 @@ def build_matrices_helper(data_filename, vocab_dict, embed_matrix):
 	news_names = [util.nypost, util.bart, util.cnn, util.wpost, util.npr]
 	n_articles = sum([len(data[news]) for news in news_names])
 	print "total # of articles: " + str(n_articles)
-	data_matrix = np.zeros((n_articles, util.glove_dimensions + 1))
+	data_matrix = np.zeros((n_articles, util.glove_dimensions + len(news_names))) # do I need this?? + len(newsnames)
 	row_num = 0	
 
 	for i, news_source in enumerate(news_names):
 		print "Adding articles from " + news_source
 		all_articles = data[news_source]
 		num_articles_so_far = 0
-		for article in all_articles:			
+		for j, article in enumerate(all_articles):
 			if num_articles_so_far % 1000 == 0:
 				print "=======>" + "Adding article " + str(num_articles_so_far)
 			indices = []
-			for token in article:
-				if token.lower() in vocab_dict:
-					indices.append(vocab_dict[token.lower()])
-				else: # OOV word, map to UNK
-					indices.append(vocab_dict[util.UNK])
-			article_avg_vec = np.mean(embed_matrix[indices], 0)
-			article_avg_vec = np.insert(article_avg_vec, util.glove_dimensions, i) #add label to end of matrix
-			data_matrix[row_num] = article_avg_vec 
-			row_num += 1
+			if len(article) > 0:
+				for token in article:
+					if token.lower() in vocab_dict:
+						indices.append(vocab_dict[token.lower()])
+					else: # OOV word, map to UNK
+						indices.append(vocab_dict[util.UNK])
+				article_avg_vec = np.mean(embed_matrix[indices], 0)
+				if np.isnan(article_avg_vec).any():
+					print "Found nan at article ", j
+					print "article vectors matrix slice ", embed_matrix[indices]
+					print article_avg_vec
+					print "article ", article
+
+				labels = np.zeros(len(news_names))
+				labels[i] = 1
+				# instead of inserting i, insert labels
+				# article_avg_vec = np.insert(article_avg_vec, util.glove_dimensions, i) 
+				article_avg_vec = np.insert(article_avg_vec, util.glove_dimensions, labels) #add label to end of matrix
+				data_matrix[row_num] = article_avg_vec 
+				row_num += 1
 			num_articles_so_far += 1
 			
 		
 
 	# shuffle matrix bc everything was added in specific order	
 	np.random.shuffle(data_matrix)
-	# split matrix into examples (x) and labels (y, last column)
-	labels = data_matrix[:,util.glove_dimensions]
+	# split matrix into examples (x) and labels (y)
+	labels = data_matrix[:,util.glove_dimensions:]
 	data_matrix = data_matrix[:,:util.glove_dimensions]
 	return data_matrix, labels		
 
@@ -96,10 +107,9 @@ def build_matrices_helper(data_filename, vocab_dict, embed_matrix):
 
 if __name__ == '__main__':
 	build_matrices("embeddings_matrix.pkl", "vocab_dict.pkl")
-	# dev_matrix = util.openPkl("dev_matrix_short.pkl")
-	# dev_labels = util.openPkl("dev_labels_short.pkl")
-	# print dev_matrix.shape
-	# print dev_labels.shape
+	dev_matrix = util.openPkl("dev_matrix_short.pkl")
+	dev_labels = util.openPkl("dev_labels_short.pkl")
+
 
 	# test_matrix = util.openPkl("test_matrix_short.pkl")
 	# test_labels = util.openPkl("test_labels_short.pkl")
