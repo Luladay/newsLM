66bbba3fccc5fb12156c167a57fd3ba223201f56
Luladay Price <luladayp@rice11.stanford.edu>
2018-03-15 21:49:01 -0700

    changed baseline and util quite a bit

--- baseline.py
+++ baseline.py
@@ -4,64 +4,14 @@ import numpy as np
 import util
 from decimal import Decimal
 
-# these imports are requiured for the confusion matrix
-import itertools
-import matplotlib
-matplotlib.use('agg')
-import matplotlib.pyplot as plt
-from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import confusion_matrix
-from sklearn.metrics import f1_score
-
-def outputConfusionMatrix(pred, labels, filename):
-    """ Generate a confusion matrix """
-
-    # print('size of pred')
-    # print(np.shape(pred))
-    # print( "size of labels")
-    # print(np.shape(labels))
-    cm = confusion_matrix(labels, pred, labels= range(5))
-    plt.figure()
-    plt.imshow(cm, interpolation='nearest', cmap=plt.cm.Reds)
-    plt.colorbar()
-	# order of classes is consitent with create_vocab_embed_matrix.py line 31
-    classes = util.classes
-    tick_marks = np.arange(len(classes))
-    plt.xticks(tick_marks, classes)
-    plt.yticks(tick_marks, classes)
-    thresh = cm.max() / 2.
-    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
-        plt.text(j, i, cm[i, j],
-                 horizontalalignment="center",
-                 color="white" if cm[i, j] > thresh else "black")
-    plt.tight_layout()
-    plt.ylabel('True label')
-    plt.xlabel('Predicted label')
-    plt.savefig(filename)    
-
-#this function can be used to test accuracy for train dev and test
-def accuracy(pred, labels):
-    """ Precision for classifier """
-    prec = 2
-    micro_f1 = f1_score(labels, pred, average="micro")
-    macro_f1 = f1_score(labels, pred, average="macro")
-    class_f1 = f1_score(labels, pred, average=None)
-    print "Micro F1 score: " + str(round(micro_f1 * 100, prec)) + "%"
-    print "Macro F1 score: " + str(round(macro_f1 * 100, prec)) + "%"
-    for class_name, score in zip(util.classes, class_f1):    	
-    	print "F1 score for " + class_name + ": ", str(round(score*100, 3)) + "%"
    
-
-def run_baseline(data_matrix, data_labels, train=True):
+def build_model(data_matrix, data_labels):
 	n_features = util.glove_dimensions
 	n_classes = 5
-	batch_size = 1000
-	n_epochs = 30
 	lr = 0.005
 
 	input_placeholder = tf.placeholder(tf.float32, shape=(None, n_features))
 	labels_placeholder = tf.placeholder(tf.int32, shape=(None, n_classes))
-
 
 	W = tf.get_variable("W", shape =[n_features, n_classes], dtype=tf.float32, initializer = tf.contrib.layers.xavier_initializer())
 	b = tf.Variable(tf.zeros([n_classes, 1], dtype=tf.float32))
@@ -72,58 +22,70 @@ def run_baseline(data_matrix, data_labels, train=True):
 	loss_op = tf.reduce_mean(loss_op, 0)
 
 	train_op = tf.train.AdamOptimizer(learning_rate = lr).minimize(loss_op)
+	return pred, input_placeholder, labels_placeholder, train_op, loss_op
 
+
+def train(data_matrix, data_labels, save_path, batch_size=100, n_epochs=30):
+	_, input_placeholder, labels_placeholder, train_op, loss_op = build_model(data_matrix, data_labels)	
+	saver = tf.train.Saver()
 	with tf.Session() as sess:
 		sess.run(tf.global_variables_initializer())
 
 		minibatches = util.get_minibatches(data_matrix, data_labels, batch_size)
 		for i in range(n_epochs):
-			if not train:
-				print "Epoch " + str(i+1) + ": "
+			print "Epoch " + str(i+1) + ": "
 			loss_list = []
 			label_list= []
 			pred_list = []
-			for tup in minibatches:
-				if train:
-					_, loss = sess.run([train_op, loss_op], feed_dict={input_placeholder: tup[0], labels_placeholder: tup[1]})
-				else:
-					pred_temp, loss, labels_temp = sess.run([pred, loss_op, labels_placeholder], feed_dict={input_placeholder: tup[0], labels_placeholder: tup[1]})
-					for i, row in enumerate(pred_temp):
-						pred_list.append(np.where(row == max(row))[0][0])
-					for i, row in enumerate(labels_temp):
-						label_list.append(np.where(row == max(row))[0][0])
-
+			for tup in minibatches:				
+				_, loss = sess.run([train_op, loss_op], feed_dict={input_placeholder: tup[0], labels_placeholder: tup[1]})
 				loss_list.append(loss)
-			print "=====>loss: " + str(np.mean(loss_list)) + " "
-			if not train:
-				break
-	return pred_list, label_list
+			print "=====>loss: " + str(np.mean(loss_list)) + " "		
+		save_path = saver.save(sess, save_path)
+  		print("Final baseline model saved in path: %s" % save_path)
 
 
+def test(data_matrix, data_labels, saved_model_path, batch_size=1000):
+	tf.reset_default_graph()
+	pred, input_placeholder, labels_placeholder, _, loss_op = build_model(data_matrix, data_labels)
+	saver = tf.train.Saver()
+	loss_list = []
+	label_list= []
+	pred_list = []
+	with tf.Session() as sess:
+		sess.run(tf.global_variables_initializer())
+		saver.restore(sess, saved_model_path)
+		print("Model restored.")
+
+		minibatches = util.get_minibatches(data_matrix, data_labels, batch_size)
+		for tup in minibatches:
+			pred_temp, loss, labels_temp = sess.run([pred, loss_op, labels_placeholder], feed_dict={input_placeholder: tup[0], labels_placeholder: tup[1]})
+			for i, row in enumerate(pred_temp):
+				pred_list.append(np.where(row == max(row))[0][0])
+			for i, row in enumerate(labels_temp):
+				label_list.append(np.where(row == max(row))[0][0])
+
+			loss_list.append(loss)
+		print "Loss: " + str(np.mean(loss_list)) + "\n"			
+
+	util.outputConfusionMatrix(pred_list, label_list, "confusion_matrix")
+	util.get_accuracy(pred_list, label_list)
 
 
 
 if __name__ == '__main__':
 	# change these filenames if the pickle files are in a Data folder
 
-	 #print "Opening train data..."
-	 #train_matrix = util.openPkl("train_matrix_short.pkl")
-	 #train_labels = util.openPkl("train_labels_short.pkl")
-	 #print "Done opening training data!"
-	 #run_baseline(train_matrix, train_labels)
+	print "Opening train data..."
+	train_matrix = util.openPkl("train_matrix_short.pkl")
+	train_labels = util.openPkl("train_labels_short.pkl")
+	print "Done opening train data!"
+	train(train_matrix, train_labels, "./models/baseline", batch_size=1000, n_epochs=70)
+
+	print "Opening test data..."
+	dev_matrix = util.openPkl("test_matrix_short.pkl")	
+	dev_labels = util.openPkl("test_labels_short.pkl")
+	print "Done opening test data!"
+	test(dev_matrix, dev_labels, "./models/baseline", batch_size=1000)
 
 
-	# print "Opening test data..."
-	# test_matrix = util.openPkl("test_matrix_short.pkl")
-	# test_labels = util.openPkl("test_labels_short.pkl")
-	# print "Done opening test data!"
-	# run_baseline(test_matrix, test_labels, train=False)
-
-
-	print "Opening dev data..."
-	dev_matrix = util.openPkl("dev_matrix_short.pkl")
-	dev_labels = util.openPkl("dev_labels_short.pkl")
-	print "Done opening dev data!"
-	pred_list, label_list = run_baseline(dev_matrix, dev_labels, train=False)
-	outputConfusionMatrix(pred_list, label_list, "confusion_matrix")
-	accuracy(pred_list, label_list)
--- util.py
+++ util.py
@@ -1,5 +1,15 @@
 import pickle
 
+# these imports are requiured for the confusion matrix
+import numpy as np
+import itertools
+import matplotlib
+matplotlib.use('agg')
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import f1_score
+
 
 UNK = '<UNK>'
 
@@ -9,6 +19,10 @@ cnn = "CNN_articles"
 wpost = "Washington_Post_articles"
 npr = "NPR_articles"
 
+classes = ["New York Post", "Breitbart", "CNN", "Washington Post", "NPR"]
+
+####### Variables #######
+
 nypost_label = 0
 bart_label = 1
 cnn_label = 2
@@ -16,10 +30,10 @@ wpost_label = 3
 npr_label = 4
 
 short_article_len = 100
-
-
 glove_dimensions = 50
 
+
+######## Useful Functions #######
 
 def dumpVar(filename, obj):
 	with open(filename, 'wb') as f:
@@ -28,4 +42,91 @@ def dumpVar(filename, obj):
 def openPkl(filename):
 	var = open(filename, "rb")
 	return pickle.load(var)
+
+def get_minibatches(data_matrix, data_labels, batch_size):
+	batch_list = []
+	indices = []
+	n_matrix_rows = data_matrix.shape[0] #dev or training examples
+	for i in range(0, n_matrix_rows, batch_size):
+		batch = data_matrix[i : i+batch_size, : ]
+		batch_label = data_labels[i : i+batch_size]
+		batch_list.append((batch, batch_label))
+	return batch_list
+
+
+def outputConfusionMatrix(pred, labels, filename):
+    """ Generate a confusion matrix """
+
+    cm = confusion_matrix(labels, pred, labels= range(5))
+    plt.figure()
+    plt.imshow(cm, interpolation='nearest', cmap=plt.cm.Reds)
+    plt.colorbar()
+	# order of classes is consitent with create_vocab_embed_matrix.py line 31
+    # classes = util.classes
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes)
+    plt.yticks(tick_marks, classes)
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, cm[i, j],
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+    plt.savefig(filename)    
+
+#this function can be used to test accuracy for train dev and test
+def get_accuracy(pred, labels):
+    """ Precision for classifier """
+    prec = 2
+    micro_f1 = f1_score(labels, pred, average="micro")
+    macro_f1 = f1_score(labels, pred, average="macro")
+    class_f1 = f1_score(labels, pred, average=None)
+    print "Micro F1 score: " + str(round(micro_f1 * 100, prec)) + "%"
+    print "Macro F1 score: " + str(round(macro_f1 * 100, prec)) + "%"
+    for class_name, score in zip(classes, class_f1):    	
+    	print "F1 score for " + class_name + ": ", str(round(score*100, 3)) + "%"
+
+
+####### Miscellaneous Functions	########
+def checkForNans():
+	print "Opening train matrix..."
+	train_matrix = openPkl("train_matrix_short.pkl")
+	print "Done opening test matrix!"
+	for i, row in enumerate(train_matrix):
+		if np.isnan(row).any():
+			print "row num: ", i
+			print "row values: ", row
+			print " "
+
+	print "Opening test_matrix matrix..."
+	test_matrix = openPkl("test_matrix_short.pkl")
+	print "Done opening test matrix!"
+	for i, row in enumerate(test_matrix):
+		if np.isnan(row).any():
+			print "row num: ", i
+			print "row values: ", row
+			print " "
+
+	print "Opening dev matrix..."
+	dev_matrix = openPkl("dev_matrix_short.pkl")
+	print "Done opening dev matrix!"
+	for i, row in enumerate(dev_matrix):
+		if np.isnan(row).any():
+			print "row num: ", i
+			print "row values: ", row
+			print " "
+
+def test_minibatches():
+	data_matrix = np.arange(20).reshape((10,2))
+	data_labels = np.arange(10)
+	batch_size = 4
+	print "data_matrix: ", data_matrix
+	print ""
+	print "data_labels: ", data_labels
+	print ""
+	batches = get_minibatches(data_matrix, data_labels, batch_size)
+	for tup in batches:
+		print tup
 
