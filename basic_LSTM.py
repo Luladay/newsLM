a4162c51e8ba1fd6796a06923df34de1de7ae5cb
Ubuntu <team@cs224-dev.hbaxscj2hl0ezo0hzaiw0jp30b.jx.internal.cloudapp.net>
2018-03-18 22:35:09 +0000

    more exps output

--- basic_LSTM.py
+++ basic_LSTM.py
@@ -33,7 +33,7 @@ def build_model(data_matrix, data_labels, hidden_size=256, lr=0.001):
 	labels_placeholder = tf.placeholder(tf.int32, shape=(None, n_classes))
 
 	# add embedding layer!
-	print "Opening embedding matrix..."
+	print "Opening embedding matrix..."	
 	embed_matrix = util.openPkl("embeddings_matrix.pkl")
 	print "Done opening embedding matrix!"
 	x = tf.nn.embedding_lookup(embed_matrix, input_placeholder)
@@ -120,7 +120,7 @@ if __name__ == '__main__':
 	train_matrix = util.openPkl("train_matrix_rnn_short.pkl")
 	train_labels = util.openPkl("train_labels_rnn_short.pkl")
 	print "Done opening train data!"
-	print ">>>>Hidden size"
+	# print ">>>>Hidden size"
 	# print "Running experiment 1..."
 	# train(train_matrix, train_labels, "./models/basic_lstm_hsize256 lr01", "Basic LSTM hsize256 lr01", 
 	# 	hidden_size=256, lr=0.001, saved_model_path="./models/basic_lstm_hsize256 lr01", RESUME=True, batch_size=256, n_epochs=5)
@@ -130,24 +130,24 @@ if __name__ == '__main__':
 	# print "Running experiment 3..."
 	# train(train_matrix, train_labels, "./models/basic_lstm_hsize512 lr01", "Basic LSTM hsize512 lr01", 
 	# 	hidden_size=512, lr=0.001, RESUME=False, batch_size=256, n_epochs=45)
-	print ">>>>Learning rate"
+	print ">>>>Learning rate pt2"
 	print "Running experiment 1..."
-	train(train_matrix, train_labels, "./models/basic_lstm_hsize256 lr05", "Basic LSTM hsize256 lr01", 
-		hidden_size=256, lr=0.005, RESUME=False, batch_size=256, n_epochs=45)
+	train(train_matrix, train_labels, "./models/basic_lstm_hsize256 lr05", "Basic LSTM hsize256 lr05", 
+		hidden_size=256, lr=0.005, RESUME=False, batch_size=256, n_epochs=30)
 	print "Running experiment 2..."
-	train(train_matrix, train_labels, "./models/basic_lstm_hsize300 lr05", "Basic LSTM hsize300 lr01", 
-		hidden_size=300, lr=0.005, RESUME=False, batch_size=256, n_epochs=45)
+	train(train_matrix, train_labels, "./models/basic_lstm_hsize300 lr05", "Basic LSTM hsize300 lr05", 
+		hidden_size=300, lr=0.005, RESUME=False, batch_size=256, n_epochs=30)
 	# print "Running experiment 3..."
-	# train(train_matrix, train_labels, "./models/basic_lstm_hsize512 lr01", "Basic LSTM hsize512 lr01", 
+	# train(train_matrix, train_labels, "./models/basic_lstm_hsize512 lr05", "Basic LSTM hsize512 lr05", 
 	# 	hidden_size=512, lr=0.005, RESUME=False, batch_size=256, n_epochs=45)
 
-	print "Opening dev data..."
-	dev_matrix = util.openPkl("dev_matrix_rnn_short.pkl")	
-	dev_labels = util.openPkl("dev_labels_rnn_short.pkl")
-	print "Done opening dev data!"
-	print "------------"
-	print "Evaluating model on hsize256 lr01 +10"
-	test(dev_matrix, dev_labels, "./models/basic_lstm_hsize256 lr01--smallest loss", "Basic LSTM hsize256 lr01", batch_size=256)
+	# print "Opening dev data..."
+	# dev_matrix = util.openPkl("dev_matrix_rnn_short.pkl")	
+	# dev_labels = util.openPkl("dev_labels_rnn_short.pkl")
+	# print "Done opening dev data!"
+	# print "------------"
+	# print "Evaluating model on hsize256 lr01 +10"
+	# test(dev_matrix, dev_labels, "./models/basic_lstm_hsize256 lr01--smallest loss", "Basic LSTM hsize256 lr01", batch_size=256)
 	# print "Evaluating model on hsize300"
 	# test(dev_matrix, dev_labels, "./models/basic_lstm_hsize300--smallest loss", "hsize300", batch_size=256)
 	# print "Evaluating model on hsize512"
