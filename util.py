df0b7ccbc7ce390069486e430333a7fb2d79be68
Ubuntu <team@cs224-dev.hbaxscj2hl0ezo0hzaiw0jp30b.jx.internal.cloudapp.net>
2018-03-19 06:28:52 +0000

    lp migrating to rice

--- util.py
+++ util.py
@@ -61,8 +61,9 @@ def get_minibatches_lm(data_matrix, batch_size):
 	indices = []
 	n_matrix_rows = data_matrix.shape[0] #dev or training examples
 	for i in range(0, n_matrix_rows, batch_size):
-		batch = data_matrix[i : i+batch_size, 0:99]
-		batch_label = (data_matrix[i : i + batch_size, -1]).astype(int)
+		batch = data_matrix[i : i+batch_size, :]
+		batch_label = batch[:, 1:]
+		batch_label = np.column_stack((batch_label, -1*np.ones(len(batch_label))))
 		batch_list.append((batch, batch_label))
 	return batch_list
 
@@ -140,18 +141,17 @@ def checkForNans():
 			print " "
 
 def test_minibatches():
-	data_matrix = np.arange(20).reshape((10,2))
-	data_labels = np.arange(10)
-	batch_size = 4
+	data_matrix = np.arange(20).reshape((4,5))
+	# data_labels = np.arange(10)
+	batch_size = 2
 	print "data_matrix: ", data_matrix
 	print ""
-	print "data_labels: ", data_labels
-	print ""
-	batches = get_minibatches(data_matrix, data_labels, batch_size)
+	# print "data_labels: ", data_labels
+	# print ""
+	batches = get_minibatches_lm(data_matrix, batch_size)
 	for tup in batches:
-		print tup
+		print "batch data", tup[0]
+		print "batch label", tup[1]
 
 if __name__ == '__main__':
-	a = [0,1,2,3,4]
-	b = [1,1,2,4,1]
-	get_accuracy(a,b)
+	test_minibatches()
